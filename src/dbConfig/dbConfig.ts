e1200619e4acf46c9285b01a88b07516d1b30436
nayeemurrehman <acenayeem01@gmail.com>
2024-02-03 15:01:41 +0530

    added singup and login page

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+    try{
+        mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+        connection.on('connected',() => {
+             console.log('mongodb connected successfully');
+
+        })
+        connection.on('error',(err) => {
+            console.log('mongodb connection error.please make sure mongodb is running' + err);
+            process.exit();
+        })
+
+    }catch (error){
+        console.log("something goes wrong");
+        console.log("error");
+    }
+    
+}
